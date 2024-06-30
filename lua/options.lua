5880eef9d7ffe0fabba957ce878f8e967d9870f9
balint.solyom <balint.solyom01@gmail.com>
2024-06-30 22:51:47 +0200

    Finished basic config, nothing language specific only basics for Lua. Later need C/C++, Java and Python setups

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,5 @@ vim.cmd("set shiftwidth=4")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s %l %r "
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
+vim.g.mapleader = " "
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
-return{
+return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-		ensure_installed = {"java", "c", "cpp", "cmake", "dockerfile", "doxygen", "json", "lua", "python", "vim", "vimdoc", "query"},
-		highlight = { enable = true },
-		indent = { enable = true },
+            auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
 		})
-	end
+	end,
 }
