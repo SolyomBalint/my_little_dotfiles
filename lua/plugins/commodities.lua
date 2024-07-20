293717e78e43b2beb432080df998bfbca3e613bf
balint.solyom <balint.solyom01@gmail.com>
2024-07-20 11:49:33 +0200

    removed whick-key plugin

--- lua/plugins/commodities.lua
+++ lua/plugins/commodities.lua
@@ -15,18 +15,4 @@ return {
 			require("Comment").setup({})
 		end,
 	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
 }
