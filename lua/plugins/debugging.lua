677ce25a380ae771fda7cb3ccf063865e6781a1e
balint.solyom <balint.solyom01@gmail.com>
2024-08-21 20:08:53 +0200

    Fxied live_grep and find_files bad ignores, removed unused git plugin and added gdb native debug for C/C++

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,4 +1,23 @@
---https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation this is where I can find debugger installiations
+-- These function may help later
+local function add_debug_keymaps()
+    local dap = require("dap")
+    vim.keymap.set("n", "<leader>dr", dap.restart, { noremap = true })
+    vim.keymap.set("n", "<leader>dt", dap.terminate, { noremap = true })
+    vim.keymap.set("n", "<F11>", dap.step_into, { noremap = true })
+    vim.keymap.set("n", "<F10>", dap.step_over, { noremap = true })
+    vim.keymap.set("n", "<F9>", dap.step_out, { noremap = true })
+    vim.keymap.set("n", "<leader>dc", dap.run_to_cursor, { noremap = true })
+end
+
+local function remove_debug_keymaps()
+    vim.keymap.del("n", "<leader>dr")
+    vim.keymap.del("n", "<leader>dt")
+    vim.keymap.del("n", "<F11>")
+    vim.keymap.del("n", "<F10>")
+    vim.keymap.del("n", "<F9>")
+    vim.keymap.del("n", "<leader>dc")
+end
+
 return {
     {
         "rcarriga/nvim-dap-ui",
@@ -14,6 +33,7 @@ return {
             "nvim-neotest/nvim-nio",
         },
         config = function()
+            -- Dap ui subscribes to dap events
             local dap, dapui = require("dap"), require("dapui")
             dap.listeners.before.attach.dapui_config = function()
                 dapui.open()
@@ -28,11 +48,14 @@ return {
                 dapui.close()
             end
 
+            -- gdb adapter setup
             dap.adapters.gdb = {
                 type = "executable",
                 command = "gdb",
                 args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
             }
+
+            --C/C++ native debugging setup
             dap.configurations.cpp = {
                 {
                     name = "Launch",
@@ -71,9 +94,15 @@ return {
 
             dap.configurations.c = dap.configurations.cpp
             vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { noremap = true })
-            vim.keymap.set("n", "<Leader>dc", dap.continue, { noremap = true })
-            vim.keymap.set("n", "<leader>di", dap.step_into, { noremap = true })
-            vim.keymap.set("n", "<leader>do", dap.step_over, { noremap = true })
+            vim.keymap.set("n", "<leader>dl", dap.run_last, { noremap = true })
+            vim.keymap.set("n", "<leader>dlb", dap.list_breakpoints, { noremap = true })
+            vim.keymap.set("n", "<F5>", dap.continue, { noremap = true })
+            vim.keymap.set("n", "<leader>dr", dap.restart, { noremap = true })
+            vim.keymap.set("n", "<leader>dt", dap.terminate, { noremap = true })
+            vim.keymap.set("n", "<F11>", dap.step_into, { noremap = true })
+            vim.keymap.set("n", "<F10>", dap.step_over, { noremap = true })
+            vim.keymap.set("n", "<F9>", dap.step_out, { noremap = true })
+            vim.keymap.set("n", "<leader>dc", dap.run_to_cursor, { noremap = true })
         end,
     },
 }
--- lua/plugins/finding_things.lua
+++ lua/plugins/finding_things.lua
@@ -1,126 +1,126 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			local utils = require("telescope.utils")
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require("telescope.builtin")
+            local utils = require("telescope.utils")
 
-			vim.keymap.set("n", "<C-p>a", builtin.find_files, {})
-			vim.keymap.set("n", "<C-p>r", function()
-			    builtin.find_files({ cwd = utils.buffer_dir() })
-			end, {})
+            vim.keymap.set("n", "<C-p>a", builtin.find_files, {})
+            vim.keymap.set("n", "<C-p>r", function()
+                builtin.find_files({ cwd = utils.buffer_dir() })
+            end, {})
 
-			vim.keymap.set("n", "<leader>fg", function()
-				builtin.live_grep({
-					additional_args = {
-						"-u",
-						"--follow",
-						"--hidden",
-						"--with-filename",
-						"--line-number",
-						"--column",
-						"--smart-case",
-						"--glob=!**/.git/*",
-						"--glob=!**/build/*",
-						"--glob=!**/.idea/*",
-					},
-				})
-			end, {})
+            vim.keymap.set("n", "<leader>fg", function()
+                builtin.live_grep({
+                    cwd = vim.fn.input("Path to basedir: ", vim.fn.getcwd() .. "/", "file"),
+                    additional_args = {
+                        "-u",
+                        "--follow",
+                        "--hidden",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                        "--glob=!**/.git/*",
+                        "--glob=!**/build/*",
+                        "--glob=!**/.idea/*",
+                    },
+                })
+            end, {})
 
             vim.keymap.set("n", "<C-p>c", builtin.current_buffer_fuzzy_find, {})
-
-			vim.keymap.set("n", "<M-g>c", builtin.git_commits, {})
-			vim.keymap.set("n", "<M-g>b", builtin.git_branches, {})
-			vim.keymap.set("n", "<M-g>s", builtin.git_status, {})
+            vim.keymap.set("n", "<M-g>c", builtin.git_commits, {})
+            vim.keymap.set("n", "<M-g>b", builtin.git_branches, {})
+            vim.keymap.set("n", "<M-g>s", builtin.git_status, {})
             vim.keymap.set("n", "<M-g>st", builtin.git_stash, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
                 defaults = {
                     file_ignore_patterns = {
-                        "build"
-                    }
+                        "build",
+                    },
                 },
-				pickers = {
-					find_files = {
-						find_command = {
-							"fdfind",
+                pickers = {
+                    find_files = {
+                        find_command = {
+                            "fdfind",
                             "--follow",
-							"--no-ignore-vcs",
-						},
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim", "telescope.nvim" },
-		config = function()
-			local harpoon = require("harpoon")
-			harpoon:setup()
+                            "--no-ignore-vcs",
+                        },
+                    },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim", "telescope.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+            harpoon:setup()
 
-			local conf = require("telescope.config").values
-			local function toggle_telescope(harpoon_files)
-				local file_paths = {}
-				for _, item in ipairs(harpoon_files.items) do
-					table.insert(file_paths, item.value)
-				end
+            local conf = require("telescope.config").values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
 
-				require("telescope.pickers")
-					.new({}, {
-						prompt_title = "Harpoon",
-						finder = require("telescope.finders").new_table({
-							results = file_paths,
-						}),
-						previewer = conf.file_previewer({}),
-						sorter = conf.generic_sorter({}),
-					})
-					:find()
-			end
+                require("telescope.pickers")
+                    .new({}, {
+                        prompt_title = "Harpoon",
+                        finder = require("telescope.finders").new_table({
+                            results = file_paths,
+                        }),
+                        previewer = conf.file_previewer({}),
+                        sorter = conf.generic_sorter({}),
+                    })
+                    :find()
+            end
 
-			vim.keymap.set("n", "<C-e>", function()
-				toggle_telescope(harpoon:list())
-			end, { desc = "Open harpoon window" })
+            vim.keymap.set("n", "<C-e>", function()
+                toggle_telescope(harpoon:list())
+            end, { desc = "Open harpoon window" })
 
-			vim.keymap.set("n", "<leader>a", function()
-				harpoon:list():add()
-			end)
-			vim.keymap.set("n", "<C-e>", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
+            vim.keymap.set("n", "<leader>a", function()
+                harpoon:list():add()
+            end)
+            vim.keymap.set("n", "<C-e>", function()
+                harpoon.ui:toggle_quick_menu(harpoon:list())
+            end)
 
-			vim.keymap.set("n", ";1", function()
-				harpoon:list():select(1)
-			end)
-			vim.keymap.set("n", ";2", function()
-				harpoon:list():select(2)
-			end)
-			vim.keymap.set("n", ";3", function()
-				harpoon:list():select(3)
-			end)
-			vim.keymap.set("n", ";4", function()
-				harpoon:list():select(4)
-			end)
+            vim.keymap.set("n", ";1", function()
+                harpoon:list():select(1)
+            end)
+            vim.keymap.set("n", ";2", function()
+                harpoon:list():select(2)
+            end)
+            vim.keymap.set("n", ";3", function()
+                harpoon:list():select(3)
+            end)
+            vim.keymap.set("n", ";4", function()
+                harpoon:list():select(4)
+            end)
 
-			vim.keymap.set("n", "<C-S-P>", function()
-				harpoon:list():prev()
-			end)
-			vim.keymap.set("n", "<C-S-N>", function()
-				harpoon:list():next()
-			end)
-		end,
-	},
+            vim.keymap.set("n", "<C-S-P>", function()
+                harpoon:list():prev()
+            end)
+            vim.keymap.set("n", "<C-S-N>", function()
+                harpoon:list():next()
+            end)
+        end,
+    },
 }
