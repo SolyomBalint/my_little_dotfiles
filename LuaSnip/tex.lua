a74a5b5e0b2bd15b57f08b4bfa7d0b22cdcedcee
balint.solyom <balint.solyom01@gmail.com>
2024-08-25 12:25:21 +0200

    added basic latex support, needs further config after understanding latex a little bit better

---
+++ LuaSnip/tex.lua
@@ -0,0 +1,4 @@
+return {
+    -- TODO later add snippets according to https://ejmastnak.com/tutorials/vim-latex/luasnip/#after-a
+    -- so far no snippets cause i have no experience with latex, so i don't have usable ides atm.
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,12 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-            auto_install = true,
-			highlight = { enable = true },
+			ignore_install = { "latex" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = { "latex" },
+			},
 			indent = { enable = true },
 		})
 	end,
