0d5c10edbc4cd08ed709b252ae6c5a3ac32e0ded
balint.solyom <balint.solyom@rollingwireless.com>
2024-08-28 09:14:55 +0200

    fixed fzf for platform

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,17 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ignore_install = { "latex" },
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = { "latex" },
-			},
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ignore_install = { "latex" },
+            ensure_installed = { "html" },
+            auto_install = true,
+            highlight = {
+                enable = true,
+                disable = { "latex" },
+            },
+            indent = { enable = true },
+        })
+    end,
 }
