22fe914aebebb240cfca655775ae1fa7b473f26e
balint.solyom <balint.solyom01@gmail.com>
2024-09-11 16:56:05 +0200

    getting rid of saturation, and adding opacity option

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,13 +11,13 @@ config.color_scheme = "tokyonight"
 config.window_background_image = "/home/solyombalint/Pictures/nvimbackground.jpg"
 
 config.hide_tab_bar_if_only_one_tab = true
+-- config.window_background_opacity = 0.9
 
 -- This is to make using neovim easier, refer ti tge keyboard concepts docs for more information.
 config.use_dead_keys = false
 
 config.window_background_image_hsb = {
     brightness = 0.1,
-    saturation = 1.0,
 }
 
 -- and finally, return the configuration to wezterm
