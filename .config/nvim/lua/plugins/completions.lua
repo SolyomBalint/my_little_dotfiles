fef0cf7e3d9652e5cee203b07ba920363869ddde
balint.solyom <balint.solyom01@gmail.com>
2024-09-22 13:53:16 +0200

    updated completions formatting

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -55,6 +55,7 @@ return {
         },
         config = function()
             local cmp = require("cmp")
+            local lspkind = require("lspkind")
             require("luasnip.loaders.from_vscode").lazy_load()
             local max_buffer_size = 1024 * 1024 -- 1 Megabyte max
 
@@ -72,7 +73,39 @@ return {
                     indexing_interval = 1000,
                 },
             }
+
             cmp.setup({
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = 'symbol_text',
+                        -- maxwidth = 50,
+                        maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                        ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                        menu = vim.tbl_extend("keep", {
+                                nvim_lsp = "[LSP]",
+                                path = "[Path]",
+                                nvim_lua = "[Lua]",
+                                buffer = "[Buffer]",
+                            },
+                            { luasnip = "[LuaSnip]" },
+                            { dictionary = "[Dictionary]" },
+                            { spell = "[Spell]" },
+                            { async_path = "{AsyncPath}" },
+                            { rg = "[RG]" },
+                            { cmdline = "[CmdLine]" },
+                            { cmdline_history = "{CmdLineHistory}" },
+                            { calc = "[Calc]" },
+                            { dap = "[DAP]" }
+                        ),
+
+                        -- The function below will be called before any actual modifications from lspkind
+                        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end
+                    })
+                },
                 snippet = {
                     expand = function(args) -- which snippet engine should be used
                         require("luasnip").lsp_expand(args.body)
