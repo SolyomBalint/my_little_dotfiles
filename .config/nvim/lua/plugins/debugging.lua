56bcfdd9d346fea2012322c803e4bf4c4799fd6d
balint.solyom <balint.solyom@rollingwireless.com>
2024-09-26 17:39:05 +0200

    added custom configs dir, and updated debugging so that launch json can be used

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -115,7 +115,14 @@ return {
             vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { noremap = true })
             vim.keymap.set("n", "<leader>dl", dap.run_last, { noremap = true })
             vim.keymap.set("n", "<leader>dlb", dap.list_breakpoints, { noremap = true })
-            vim.keymap.set("n", "<F5>", dap.continue, { noremap = true })
+            vim.keymap.set("n", "<F5>",
+                function()
+                    if vim.fn.filereadable("~/.config/custom/launch.json") then
+                        require('dep.ext.vscode').load_launchjs("~/.config/custom/launch.json")
+                    end
+                    dap.continue()
+                end,
+                { noremap = true })
         end,
     },
     {
@@ -133,15 +140,15 @@ return {
         dependencies = { "rcarriga/nvim-dap-ui", "nvim-treesitter/nvim-treesitter" },
         config = function()
             require("nvim-dap-virtual-text").setup {
-                enabled = true,         -- enable this plugin (the default)
-                enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                enabled = true,                     -- enable this plugin (the default)
+                enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
                 highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-                highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-                show_stop_reason = true, -- show stop reason when stopped for exceptions
-                commented = false,      -- prefix virtual text with comment string
-                only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
-                all_references = false, -- show virtual text on all all references of the variable (not only definitions)
-                clear_on_continue = false, -- clear virtual text on "continue" (might cause flickering when stepping)
+                highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                show_stop_reason = true,            -- show stop reason when stopped for exceptions
+                commented = false,                  -- prefix virtual text with comment string
+                only_first_definition = true,       -- only show virtual text at first definition (if there are multiple)
+                all_references = false,             -- show virtual text on all all references of the variable (not only definitions)
+                clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
                 --- A callback that determines how a variable is displayed or whether it should be omitted
                 --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
                 --- @param buf number
@@ -161,8 +168,8 @@ return {
                 virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
 
                 -- experimental features:
-                all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-                virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+                all_frames = false,     -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                virt_lines = false,     -- show virtual lines instead of virtual text (will flicker!)
                 virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
                 -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
             }
