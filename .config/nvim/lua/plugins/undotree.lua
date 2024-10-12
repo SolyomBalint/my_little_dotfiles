227f5d8393a20580c060ec8546bce34a051a4584
balint.solyom <balint.solyom01@gmail.com>
2024-10-12 21:15:54 +0200

    add undo tree and ingore persistency file

---
+++ .config/nvim/lua/plugins/undotree.lua
@@ -0,0 +1,16 @@
+return {
+    "mbbill/undotree",
+    config = function()
+        vim.opt.undodir = vim.fn.stdpath("config") .. "/.undo"
+        vim.opt.undofile = true
+
+        local undodir = vim.fn.stdpath("config") .. "/.undo"
+        if vim.fn.isdirectory(undodir) == 0 then
+            vim.fn.mkdir(undodir, "p")
+        end
+
+        vim.g.undotree_WindowLayout = 4
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+    end,
+}
