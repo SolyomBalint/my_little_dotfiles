a4023d2009a6cf6a3da694c76d1f0b75286ad89e
SolyomBalint <balint.solyom01@gmail.com>
2024-12-13 22:57:53 +0100

    Fix fuck up

--- nixos_config/hosts/zephyrus/default.nix
+++ nixos_config/hosts/zephyrus/default.nix
@@ -21,7 +21,7 @@
   networking.networkmanager.enable = true;
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
