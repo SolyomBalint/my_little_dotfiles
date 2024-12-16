2a3ab42cd24ddc3eb97015b83280eabfcd57f8d2
SolyomBalint <balint.solyom01@gmail.com>
2024-12-16 21:01:45 +0100

    Adding aws

--- nixos_config/hosts/zephyrus/default.nix
+++ nixos_config/hosts/zephyrus/default.nix
@@ -2,11 +2,12 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
     ../../common_modules/common_system.nix
     ../../common_modules/desktop_envs/gnome.nix
     ../../common_modules/desktop_envs/hyprland.nix
+    ../../common_modules/widget_frameworks/ags.nix
 
+    # Include the results of the hardware scan.
     ./hardware-configuration.nix
     inputs.nixos-hardware.nixosModules.asus-zephyrus-ga402
   ];
