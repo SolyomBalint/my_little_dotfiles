0990538a6b53dea07d88f89b4f9bb6ecbdead5a8
SolyomBalint <balint.solyom01@gmail.com>
2025-04-27 10:14:38 +0200

    Update all lock files

--- .config/nvim/lua/plugins/latex.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "lervag/vimtex",
-        lazy = false, -- The documentation suggests to avoid lazy loading this one
-        init = function()
-            vim.g.vimtex_view_method = "zathura"
-        end,
-    },
-}
